70c0401c843a1cb015952b41a1b35b9f2fc46e1a
seokang <seokang91@gmail.com>
2022-01-14 01:00:52 +0900

    [ADD] textfield options ( KeyboardOption/Action/Maxlines..)

--- app/src/main/java/me/seokang/compose/MainActivity.kt
+++ app/src/main/java/me/seokang/compose/MainActivity.kt
@@ -1,110 +1,24 @@
 package me.seokang.compose
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import me.seokang.compose.data.Message
-import me.seokang.compose.data.SampleData
+import androidx.compose.ui.ExperimentalComposeUiApi
+import me.seokang.compose.ui.LoginScreen
 import me.seokang.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposeTheme {
-                Conversation(messages = SampleData.conversationSample)
+                LoginScreen()
             }
         }
     }
 }
 
 
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Conversation(messages = SampleData.conversationSample)
-    }
-}
-
-@Composable
-fun Message(name: String) {
-    Text(
-        text = name,
-        color = MaterialTheme.colors.secondaryVariant,
-        style = MaterialTheme.typography.subtitle2
-    )
-}
-
-@Composable
-fun MessageArea(text: String, isExpanded: Boolean) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        elevation = 1.dp
-    ) {
-        Text(
-            text = "Input text = ($text)",
-            color = MaterialTheme.colors.secondaryVariant,
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.padding(all = 4.dp),
-            maxLines = if (isExpanded) Int.MAX_VALUE else 1 // If the message is expanded, we display all its content otherwise we only display the first line
-        )
-    }
-}
-
-@Composable
-fun MessageCard(message: Message) {
-    Row(
-        modifier = Modifier.padding(all = 8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Profile Image",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape) //Set image size to 40 dp and shaped as a circle
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))  // Add a horizontal space between the image and the column
-
-        var isExpanded by remember { mutableStateOf(false) } // We keep track if the message is expanded or not in this variable
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) { // We toggle the isExpanded variable when we click on this Column
-            Message(name = message.title)
-            Spacer(modifier = Modifier.height(4.dp)) // Add a vertical space between the Greeting and MessageCard texts
-            MessageArea(text = message.body, isExpanded = isExpanded)
-        }
-    }
-}
-
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message -> MessageCard(message = message) }
-    }
-}
--- app/src/main/java/me/seokang/compose/ui/Login.kt
+++ app/src/main/java/me/seokang/compose/ui/Login.kt
@@ -1,20 +1,31 @@
 package me.seokang.compose.ui
 
+import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
@@ -25,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import me.seokang.compose.ui.theme.ComposeTheme
 
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun LoginPreview() {
@@ -34,6 +46,7 @@ fun LoginPreview() {
 }
 
 
+@ExperimentalComposeUiApi
 @Composable
 fun LoginScreen() {
     val context = LocalContext.current
@@ -41,10 +54,16 @@ fun LoginScreen() {
     val emailErrorState = remember { mutableStateOf(false) }
     val passwordErrorState = remember { mutableStateOf(false) }
     val password = remember { mutableStateOf(TextFieldValue()) }
+    val focusRequest = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { keyboardController?.hide() })
+            },
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -69,14 +88,17 @@ fun LoginScreen() {
             },
             isError = emailErrorState.value,
             modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = "Enter Email")
-            },
+            label = { Text(text = "Enter Email") },
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { focusRequest.requestFocus() })
         )
         if (emailErrorState.value) {
             Text(text = "Required", color = Color.Red)
         }
+
         Spacer(Modifier.size(16.dp))
+
         val passwordVisibility = remember { mutableStateOf(true) }
         OutlinedTextField(
             value = password.value,
@@ -87,7 +109,9 @@ fun LoginScreen() {
                 password.value = it
             },
             isError = passwordErrorState.value,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequest),
             label = {
                 Text(text = "Enter Password")
             },
@@ -102,7 +126,12 @@ fun LoginScreen() {
                     )
                 }
             },
-            visualTransformation = if (passwordVisibility.value) PasswordVisualTransformation() else VisualTransformation.None
+            visualTransformation = if (passwordVisibility.value) PasswordVisualTransformation() else VisualTransformation.None,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                keyboardController?.hide()
+                login(context, email, emailErrorState, password, passwordErrorState)
+            })
         )
         if (passwordErrorState.value) {
             Text(text = "Required", color = Color.Red)
@@ -110,24 +139,7 @@ fun LoginScreen() {
         Spacer(Modifier.size(16.dp))
         Button(
             onClick = {
-                when {
-                    email.value.text.isEmpty() -> {
-                        emailErrorState.value = true
-                    }
-                    password.value.text.isEmpty() -> {
-                        passwordErrorState.value = true
-                    }
-                    else -> {
-                        passwordErrorState.value = false
-                        emailErrorState.value = false
-                        Toast.makeText(
-                            context,
-                            "Logged in successfully",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
+                login(context, email, emailErrorState, password, passwordErrorState)
             },
             content = {
                 Text(text = "Login", color = Color.White)
@@ -142,6 +154,29 @@ fun LoginScreen() {
             }) {
                 Text(text = "Register ?", color = Color.Red)
             }
+        }
+    }
+}
+
+private fun login(
+    context: Context, email: MutableState<TextFieldValue>, emailErrorState: MutableState<Boolean>,
+    password: MutableState<TextFieldValue>, passwordErrorState: MutableState<Boolean>
+) {
+    when {
+        email.value.text.isEmpty() -> {
+            emailErrorState.value = true
+        }
+        password.value.text.isEmpty() -> {
+            passwordErrorState.value = true
+        }
+        else -> {
+            passwordErrorState.value = false
+            emailErrorState.value = false
+            Toast.makeText(
+                context,
+                "Logged in successfully",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
